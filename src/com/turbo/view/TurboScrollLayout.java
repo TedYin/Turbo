a8b71f11c3195e24ec6a724a7349c5d14472baa6
TedYin <water-cs@qq.com>
2014-01-21 22:31:18 +0800

    添加自定义ScrollLayout的封装

---
+++ src/com/turbo/view/TurboScrollLayout.java
@@ -0,0 +1,193 @@
+package com.turbo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+/**
+ * 横向滑动控件
+ * 
+ * @author Ted
+ */
+public class TurboScrollLayout extends ViewGroup {
+
+	private static final String TAG = "TurboScrollLayout";
+	private VelocityTracker mVelocityTracker; // 用于判断滑动手势速度
+	private static final int SNAP_VELOCITY = 600; // 滑动速度临界值
+	private Scroller mScroller; // 滑动控制器
+	private int mCurScreen = 0; // 当前屏幕(默认第一屏的位置为0)
+	private float mLastMotionX; // 前一个View的X坐标
+	private OnViewChangeListener listener; // 滑动监听器
+
+	/**
+	 * ScrollLayout滑动监听
+	 * 
+	 * @author Ted
+	 */
+	public interface OnViewChangeListener {
+		/**
+		 * 切换到目标View
+		 * 
+		 * @param destViewPosition
+		 */
+		public void onViewChange(int destViewPosition);
+	}
+
+	public TurboScrollLayout(Context context) {
+		super(context);
+		init(context);
+	}
+
+	public TurboScrollLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(context);
+	}
+
+	public TurboScrollLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(context);
+	}
+
+	private void init(Context context) {
+		mScroller = new Scroller(context);
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		if (changed) {
+			int childLeft = 0;
+			for (int i = 0; i < getChildCount(); i++) {
+				final View childView = getChildAt(i);
+				if (childView.getVisibility() != View.GONE) {
+					final int childWidth = childView.getMeasuredWidth();
+					childView.layout(childLeft, 0, childLeft + childWidth,
+							childView.getMeasuredHeight());
+					childLeft += childWidth;
+				}
+			}
+		}
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		final int width = MeasureSpec.getSize(widthMeasureSpec);
+		for (int i = 0; i < getChildCount(); i++) {
+			getChildAt(i).measure(widthMeasureSpec, heightMeasureSpec);
+		}
+		scrollTo(mCurScreen * width, 0);
+	}
+
+	@Override
+	public void computeScroll() {
+		if (mScroller.computeScrollOffset()) {
+			scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+			postInvalidate();
+		}
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		final int action = event.getAction();
+		final float x = event.getX();
+		switch (action) {
+		case MotionEvent.ACTION_DOWN:
+			if (mVelocityTracker == null) {
+				mVelocityTracker = VelocityTracker.obtain();
+				mVelocityTracker.addMovement(event);
+			}
+			if (!mScroller.isFinished()) {
+				mScroller.abortAnimation();
+			}
+			mLastMotionX = x;
+			break;
+
+		case MotionEvent.ACTION_MOVE:
+			int deltaX = (int) (mLastMotionX - x);
+			if (IsCanMove(deltaX)) {
+				if (mVelocityTracker != null) {
+					mVelocityTracker.addMovement(event);
+				}
+				mLastMotionX = x;
+				scrollBy(deltaX, 0);
+			}
+
+			break;
+		case MotionEvent.ACTION_UP:
+			int velocityX = 0;
+			if (mVelocityTracker != null) {
+				mVelocityTracker.addMovement(event);
+				mVelocityTracker.computeCurrentVelocity(1000);
+				velocityX = (int) mVelocityTracker.getXVelocity();
+			}
+			if (velocityX > SNAP_VELOCITY && mCurScreen > 0) {
+				// Fling enough to move left
+				Log.e(TAG, "snap left");
+				snapToScreen(mCurScreen - 1);
+			} else if (velocityX < -SNAP_VELOCITY
+					&& mCurScreen < getChildCount() - 1) {
+				// Fling enough to move right
+				Log.e(TAG, "snap right");
+				snapToScreen(mCurScreen + 1);
+			} else {
+				int screenWidth = getWidth();
+				int destScreen = (getScrollX() + screenWidth / 2) / screenWidth;
+				snapToScreen(destScreen);
+			}
+
+			if (mVelocityTracker != null) {
+				mVelocityTracker.recycle();
+				mVelocityTracker = null;
+			}
+			break;
+		}
+		return true;
+	}
+	
+	/**
+	 * 是否可以继续滚动
+	 * @param deltaX
+	 * @return
+	 */
+	private boolean IsCanMove(int deltaX) {
+		if (getScrollX() <= 0 && deltaX < 0) 
+			return false;
+		if (getScrollX() >= (getChildCount() - 1) * getWidth() && deltaX > 0) 
+			return false;
+		return true;
+	}
+	
+	/**
+	 * 滑动到指定的Screen
+	 * 
+	 * @param whichScreen
+	 */
+	public void snapToScreen(int whichScreen) {
+		// get the valid layout page
+		whichScreen = Math.max(0, Math.min(whichScreen, getChildCount() - 1));
+		if (getScrollX() != (whichScreen * getWidth())) {
+			final int delta = whichScreen * getWidth() - getScrollX();
+			mScroller.startScroll(getScrollX(), 0, delta, 0,
+					Math.abs(delta) * 2);
+
+			mCurScreen = whichScreen;
+			invalidate(); // Redraw the layout
+			if (listener != null) {
+				listener.onViewChange(mCurScreen);
+			}
+		}
+	}
+	
+	/**
+	 * 添加监听器
+	 * @param listener
+	 */
+	public void addOnViewChangeListener(OnViewChangeListener listener) {
+		this.listener = listener;
+	}
+}
---
+++ src/com/turbo/view/TurboScrollViewHelper.java
@@ -0,0 +1,221 @@
+package com.turbo.view;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+
+import com.tedyin.turbo.R;
+import com.turbo.view.TurboScrollLayout.OnViewChangeListener;
+
+import java.util.Map;
+
+/**
+ * ScrollView 配置类
+ * 
+ * @author Ted
+ */
+public class TurboScrollViewHelper implements OnViewChangeListener, OnClickListener {
+
+	private Context context;
+	private TurboScrollLayout scrollLayout; // 滑动布局
+	private LinearLayout dotLayout; // 底部原点布局
+	private FrameLayout parentLayout; // 父Layout，承载scrollLayout和dotLayout
+	private int[] viewBgRes;
+	private ImageView[] mImageViews;
+	private int mCurSel = 0; // 当前选中的View
+	private int viewCount = 0; // View的数量
+
+	/**
+	 * 构造函数
+	 * 
+	 * @param context
+	 * @param viewBgRes
+	 */
+	public TurboScrollViewHelper(Context context, int[] viewBgRes) {
+		this.context = context;
+		this.viewBgRes = viewBgRes;
+		this.viewCount = viewBgRes.length;
+		initLayout();
+	}
+
+	/**
+	 * 初始化Layout布局
+	 */
+	private void initLayout() {
+		// init Scroll Layout
+		scrollLayout = new TurboScrollLayout(context);
+		scrollLayout.addOnViewChangeListener(this);
+		scrollLayout.setMinimumHeight(LayoutParams.MATCH_PARENT);
+		scrollLayout.setMinimumWidth(LayoutParams.MATCH_PARENT);
+
+		// init Dot layout
+		dotLayout = new LinearLayout(context);
+		LinearLayout.LayoutParams dotLP = new LinearLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		dotLayout.setOrientation(LinearLayout.HORIZONTAL);
+		dotLayout.setLayoutParams(dotLP);
+
+		// init parent layout
+		parentLayout = new FrameLayout(context);
+		// RelativeLayout.LayoutParams parentLP = new
+		// RelativeLayout.LayoutParams(
+		// LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		FrameLayout.LayoutParams parentLP = new FrameLayout.LayoutParams(
+				LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		parentLayout.setLayoutParams(parentLP);
+	}
+
+	@Override
+	public void onViewChange(int destViewPosition) {
+		setCurPoint(destViewPosition);
+	}
+
+	public void onClick(View v) {
+		int pos = (Integer) (v.getTag());
+		setCurPoint(pos);
+		scrollLayout.snapToScreen(pos);
+	}
+
+	/**
+	 * 初始化ScrollLayout部分
+	 * 
+	 * @param viewBgRes
+	 */
+	private void initScrollLayout(int[] viewBgRes) {
+		for (int i = 0; i < viewBgRes.length; i++) {
+			FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
+					LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+			FrameLayout layout = new FrameLayout(context);
+			layout.setBackgroundResource(viewBgRes[i]);
+			layout.setLayoutParams(lp);
+			scrollLayout.addView(layout);
+		}
+	}
+
+	/**
+	 * 自定义最后一个View
+	 * 
+	 * @param lastLayoutId
+	 *            最后一个View的Id
+	 * @param btnMap
+	 *            Map<按钮ID，按钮监听器>
+	 */
+	private void initScrollLayout(int lastLayoutId,
+			Map<Integer, OnClickListener> btnMap) {
+		initScrollLayout(viewBgRes);
+		LayoutInflater inflater = LayoutInflater.from(context);
+		View lastLayout = inflater.inflate(lastLayoutId, null);
+		for (int btnId : btnMap.keySet()) {
+			lastLayout.findViewById(btnId)
+					.setOnClickListener(btnMap.get(btnId));
+		}
+		scrollLayout.addView(lastLayout);
+	}
+
+	/**
+	 * 初始化Dot部分
+	 * 
+	 * @param viewCount
+	 */
+	private void initDotLayout(int viewCount) {
+		mImageViews = new ImageView[viewCount];
+		LinearLayout.LayoutParams imageLp = new LinearLayout.LayoutParams(
+				LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		int margin = px2dip(40);
+		int padding = px2dip(15);
+		imageLp.setMargins(margin, margin, margin, margin);
+		imageLp.gravity = Gravity.CENTER_VERTICAL;
+		for (int i = 0; i < viewCount; i++) {
+			ImageView image = new ImageView(context);
+			image.setBackgroundResource(R.drawable.turbo_scroll_view_dot_bg);
+			image.setPadding(padding, padding, padding, padding);
+			image.setClickable(true);
+			image.setTag(i);
+			image.setOnClickListener(this);
+			mImageViews[i] = image;
+			dotLayout.addView(image, imageLp);
+		}
+		mImageViews[0].setEnabled(false);
+	}
+
+	/**
+	 * 设置当前Dot的位置
+	 * 
+	 * @param index
+	 */
+	private void setCurPoint(int index) {
+		if (index < 0 || index > viewCount - 1 || mCurSel == index) {
+			return;
+		}
+		mImageViews[mCurSel].setEnabled(true);
+		mImageViews[index].setEnabled(false);
+		mCurSel = index;
+		if (index == viewCount - 1) {
+			dotLayout.setVisibility(View.GONE);
+		} else {
+			dotLayout.setVisibility(View.VISIBLE);
+		}
+	}
+
+	/**
+	 * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+	 */
+	private int px2dip(float pxValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (pxValue / scale + 0.5f);
+	}
+
+	/**
+	 * 创建滑动布局
+	 * 
+	 * @return
+	 */
+	public View createView() {
+		initScrollLayout(viewBgRes);
+		initDotLayout(viewBgRes.length);
+		// 添加ScrollLayout
+		FrameLayout.LayoutParams scrollParams = new FrameLayout.LayoutParams(
+				LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		parentLayout.addView(scrollLayout, scrollParams);
+		// 添加DotLayout
+		FrameLayout.LayoutParams dotParams = new FrameLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		dotParams.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
+		dotParams.setMargins(0, 0, 0, px2dip(30));
+		parentLayout.addView(dotLayout, dotParams);
+		return parentLayout;
+	}
+
+	/**
+	 * 创建滑动布局
+	 * 
+	 * @param lastLayoutId
+	 * @param btnMap
+	 *            Map<按钮ID，按钮监听器>
+	 * @return
+	 */
+	public View createView(int lastLayoutId,
+			Map<Integer, OnClickListener> btnMap) {
+		// 添加最后一个自定义View
+		viewCount += 1;
+		initScrollLayout(lastLayoutId, btnMap);
+		initDotLayout(viewBgRes.length + 1);
+		// 添加ScrollLayout
+		FrameLayout.LayoutParams scrollParams = new FrameLayout.LayoutParams(
+				LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		parentLayout.addView(scrollLayout, scrollParams);
+		// 添加DotLayout
+		FrameLayout.LayoutParams dotParams = new FrameLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		dotParams.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
+		dotParams.setMargins(0, 0, 0, px2dip(30));
+		parentLayout.addView(dotLayout, dotParams);
+		return parentLayout;
+	}
+}
